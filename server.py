020a2a3600112be15cfeb96e7fad41cfe3d51ffc
konradluberapolsl <44377516+konradluberapolsl@users.noreply.github.com>
2019-10-24 20:00:20 +0200

    ecnryption is working

--- server.py
+++ server.py
@@ -6,9 +6,16 @@ def fread(nazwa):
     file.close()
 
 
-def fwrite(name):
-    file = open(name, 'r')
-    list = file.readlines()
+def fwrite(name, list):
+    file = open(name, 'w')
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            for n in range(len(list[i][j])):
+                file.write(str(list[i][j][n]))
+                if n==len(list[i][j])-1:
+                    file.write(" ")
+            if j==len(list[i])-1:
+                file.write('\n')
     file.close()
 
 
@@ -18,18 +25,26 @@ def encrypt(list_encrypt):
         encrypted.append([])
         for j in range(len(list_encrypt[i])):
             if list_encrypt[i][j] != '\n':
-                if j!=0:
-                    print("chuj")
+                if j != 0:
+                    sys = (ord(list_encrypt[i][j - 1]) % 8) + 2
+                    encrypted[i].append(conversion(ord(list_encrypt[i][j]), sys))
+
                 else:
-                    print("chuj 1. litera")
-                #print(bin(ord(list_encrypt[i][j])))
-                print(list_encrypt[i][j])
-                encrypted[i].append(list_encrypt[i][j])
+                    encrypted[i].append(conversion(ord(list_encrypt[i][j]), 2))
     print(encrypted)
+    fwrite("input\\szyfr.txt", encrypted)
 
 
-#def conversion(number, system):
+def conversion(number, system):
+    converted = []
+    n = number
+    s = system
+    while n > 0:
+        converted.append(n % s)
+        n = int(n / s)
+    converted.reverse()
+    return converted
 
 
 
-fread("input\slowa.txt")
+fread("input\\tekst.txt")
